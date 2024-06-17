94482818cc930415337dd78273472ad4571b8db8
Kingsong97 <161429740+Kingsong97@users.noreply.github.com>
2024-06-17 18:05:06 +0900

    fix

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,8 +6,8 @@ const Home = () => {
         <Main title='Home page' description='Home page'>
             <section id='home' className='home-section'>
                 <div className='content'>
-                    <h1>Welcome to the Home Page</h1>
-                    <p>Explore our amazing collection of videos and enjoy your time here.</p>
+                    <h1>홈페이지에 오신 것을 환영합니다</h1>
+                    <p>다양한 헬스 정보를 시청하고 즐거운 시간을 보내세요.</p>
                     <button onClick={() => window.location.href = '/search/운동'}>운동 관련 영상 보기</button>
                 </div>
             </section>
